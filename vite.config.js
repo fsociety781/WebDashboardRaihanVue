54f09011922e8ec612bef5d823aad7134e1ab284
Raihan Muhammad Saprudin <60098893+fsociety781@users.noreply.github.com>
2023-06-11 08:27:55 +0700

    github seeting

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dashboard-todolist/",
+  base: "https://github.com/fsociety781/WebDashboardRaihanVue.io",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
