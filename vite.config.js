ec95aaff2a0efe775a7d552ff77597ada13abf04
Raihan Muhammad Saprudin <60098893+fsociety781@users.noreply.github.com>
2023-06-11 08:35:47 +0700

    github setting

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/fsociety781/WebDashboardRaihanVue.io",
+  base: "https://fsociety781.github.io/WebDashboardRaihanVue/",
   plugins: [vue()],
   resolve: {
     alias: {
